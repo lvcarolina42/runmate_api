5344bfcdbaca16a95054093fc3f35e1d797e47ef
lvcarolina42 <lvcarolina42@gmail.com>
2025-06-23 22:08:37 -0300

    try2fix: cors errors

--- cmd/main.go
+++ cmd/main.go
@@ -14,6 +14,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -69,6 +70,13 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger, middleware.RealIP, middleware.Recoverer, middleware.RequestID)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	adm.Routes(r)
 	api.Routes(r)
